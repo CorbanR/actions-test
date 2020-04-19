b5c72080885b87faebbdfbd4bb5bc9068d9a0dc4
Corban Raun <craun@instructure.com>
2020-04-19 13:22:57 -0600

    test nix updates

---
+++ tests/nix/default.nix
@@ -0,0 +1,11 @@
+{ system ? builtins.currentSystem }:
+
+let
+  pkgs = import <nixpkgs> { inherit system; };
+
+  callPackage = pkgs.lib.callPackageWith (pkgs // self);
+
+  self = {
+    hello = callPackage ./hello { };
+  };
+in self
